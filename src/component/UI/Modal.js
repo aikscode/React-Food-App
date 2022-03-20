cc0dfb0c3c2356ae8ea16c89083ef638ef8e3e19
Aikins Dwamena <aikinsakenten@gmail.com>
2022-03-20 10:29:38 -0700

    Practiced basic react concepts

---
+++ src/component/UI/Modal.js
@@ -0,0 +1,29 @@
+import { createPortal } from "react-dom";
+import { Fragment } from "react/cjs/react.production.min";
+import classes from "./Modal.module.css";
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
